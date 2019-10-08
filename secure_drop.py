8c880694cb4c7fe5797f4ca84ff3523e8acdc5a2
cperkins <carltonrp52@gmail.com>
2019-10-08 14:28:20 -0400

    Document code

--- secure_drop.py
+++ secure_drop.py
@@ -1,22 +1,32 @@
 #!/usr/bin/env python3
 
 # import os
-import cmd, pathlib, configparser, getpass, crypt
-from hmac import compare_digest as comp_hash
+import cmd # Supplies the subshell CMD prompt
+import pathlib # Sane Path accessors/Validators
+import configparser # .ini-ish file config accessor/generators
+import getpass # No echo prompt for password
+import crypt # Many Crypt functions
 
+# Constant time Hash comepare that prevents the raw from ever being in memory
+from hmac import compare_digest as comp_hash 
+
+# Default config directory
 CONFIGFILE = pathlib.Path('~/.secure_drop/config').expanduser()
 
 def main():
-    print('Something')
+    # Run Registration if not already done
     if (not isRegistered()):
         register()
         cleanUpAndExit()
 
+    # Ask the user for login credentials
     # login()
 
+    # Enter Custom SubShell defined be Shell class
     Shell().cmdloop()
     
-
+# Custom Shell class, Defines options avaiable from command line
+# Has built in help command which prints the text in first line
 class Shell(cmd.Cmd):
     intro = ''
     prompt = 'Secure_Drop> '
@@ -26,26 +36,34 @@ class Shell(cmd.Cmd):
         cleanUpAndExit()
     def do_add(self, args):
         'Add a new contact'
-        print('Add')
+        print('Add') # TODO stub
     def do_list(self, args):
         'List all online contacts'
-        print('list')
+        print('list') # TODO stub
     def do_send(self, args):
         'Transfer a file to contact'
-        print('send')
+        print('send') # TODO stub
 
+
+# Find if a user is already registered
+# Done by checking the config file and seeing if it contains credentials
 def isRegistered():
-    config = getConfig()
+    config = readConfig()
 
+    # Ask for forgiveness not permission
+    # ? Only checks for the existence of credentials not the validity of credentials,
+    # ? is this good enough?
     try:
         config['Cred']['name']
         config['Cred']['email']
         config['Cred']['password']
 
         return True
-    except KeyError:
+    except KeyError: # Key does not exist in file
         return False
     
+
+# Prompt the user for registration credentials, save to config if valid
 def register():
     print('Register')
 
@@ -57,7 +75,8 @@ def register():
 
     print('Registration Successful')
 
-    config = getConfig()
+    # Save credentials to config
+    config = readConfig()
 
     config['Cred'] = {
         'name':name,
@@ -72,10 +91,11 @@ def checkPassword(plaintext, crypt):
 def cryptPassword(plaintext):
     # Crypt will always use strongest salt available
     return crypt.crypt(plaintext)
+
 def cryptPasswordSalted(plaintext, salt):
     return crypt.crypt(plaintext, salt)
 
-def getConfig():
+def readConfig():
     config = configparser.ConfigParser()
     if (CONFIGFILE.exists()):
         with open(CONFIGFILE, "r") as fp:
@@ -86,10 +106,13 @@ def writeConfig(config):
     with open(CONFIGFILE, "w+") as fp:
         config.write(fp)
     
+# Clean exit of subshell
 def cleanUpAndExit():
     print('Exit SecureDrop')
     exit(0)
 
+# Check to see if we are in a module or the main application
+# If main, run main
 if __name__ == "__main__":
     try:
         main()
