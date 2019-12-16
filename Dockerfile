b4f00b5045a4fe569c6489b7170effdb93437e06
cperkins <carltonrp52@gmail.com>
2019-12-16 18:05:00 -0500

    WIP netfiletransfer

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,11 @@ FROM python:latest
 
 WORKDIR /SecureDrop/
 
-COPY *.py ./
 COPY *.txt ./
+RUN pip3 install -r ./requirements.txt
+
+COPY *.py ./
 COPY testConfigs/ ./testConfigs
 
-RUN pip3 install -r ./requirements.txt
 
 CMD bash
--- netFileTransfer.py
+++ netFileTransfer.py
@@ -14,27 +14,35 @@ blocksize = 8192
 # print(port + " " + ip + " " + file)
 
 conn = socket.socket()
+conn.settimeout(5)
 
 if (mode == 'write'):
-    conn.bind((ip, port))
+    while True:
+        conn.connect((ip, port))
+        print("loop")
 
-    if os.path.exists(file):
-        with open(file, 'rb') as f:
-            packet = f.read(blocksize)
-
-            while packet != '':
-                conn.send(packet)
-
+        if os.path.exists(file):
+            with open(file, 'rb') as f:
                 packet = f.read(blocksize)
+
+                conn.sendall(packet)
+                print("Packet done")
+
+                conn.close()
+                break
 elif (mode == 'read'):
     conn.bind((ip, port))
     conn.listen(10)
 
     c, addr = conn.accept()
+    print('{} connected.'.format(addr))
     
     while True:
-        data = conn.recv(1024)
-        if data: 
-            print(data)
+        data = c.recv(blocksize)
+        print(data)
+
+        if not data: 
+            print("Done")
+            break
 
 
